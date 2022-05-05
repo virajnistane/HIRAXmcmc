216659cf90af709d06092520007dd9510212cad0
Viraj Nistane <viraj.nistane@unige.ch>
2022-05-05 11:49:15 +0200

    requirements.txt added, currently not added to setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
 
 
+with open('requirements.txt', 'r') as fh:
+    requires = fh.read().split()
 
 
 setup(
@@ -10,7 +12,8 @@ setup(
     description='MCMC pipeline for parameter estimation from 21cm PS in HIRAX m-mode simulations',
     author='Viraj Nistane',
     license='MIT',
-    install_requires=[],
+    ## install_requires=requires,
+    install_requires = [],
     include_package_data=True,
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
