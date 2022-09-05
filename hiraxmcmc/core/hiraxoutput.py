6d364a7c22145e7aebc9840bf79f781ca0be1663
Viraj Nistane <viraj.nistane@unige.ch>
2022-09-05 18:29:12 +0900

    no change

--- hiraxmcmc/core/hiraxoutput.py
+++ hiraxmcmc/core/hiraxoutput.py
@@ -64,8 +64,7 @@ class HiraxOutput:
             M = np.diag(self.cinv.T.reshape(int(self.cinv.shape[0]*self.cinv.shape[1]),int(self.cinv.shape[2]*self.cinv.shape[3])).sum(axis=1)** -1)
             covhirax = np.matmul(M, np.matmul(self.cinv.T.reshape(int(self.cinv.shape[0]*self.cinv.shape[1]),int(self.cinv.shape[2]*self.cinv.shape[3])), M.T))
         # return 0.05**2 * np.identity(len(covhirax)) # 
-        covhirax1 = covhirax #/ self.h**3
-        return covhirax1
+        return covhirax
     
     # @property
     # def covhirax(self):
@@ -144,9 +143,7 @@ class HiraxOutput:
         
         errs = np.sqrt(abs(np.diag(self.covhirax))).reshape(kpar_params['kpar_size'],kperp_params['kperp_size'])
         
-        errs1 = errs # / self.h**(3/2)
-        
-        return errs1 
+        return errs
         
     
     # def k_space_parameters_al(self):
