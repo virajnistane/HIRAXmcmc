7047c142b925e57797fe53d152e63d0892f5bf80
Viraj Nistane <viraj.nistane@unige.ch>
2023-02-13 23:05:54 +0100

    minor changes relevant to disabling TE and EE spectra inclusion in planck likelihood

--- hiraxmcmc/scripts/mcmc.py
+++ hiraxmcmc/scripts/mcmc.py
@@ -746,13 +746,13 @@ for freqc,val in inputforhiraxoutput.items():
 
 ls = CLASS_instance_current.raw_cl()['ell'][2:]
 Cltt = CLASS_instance_current.raw_cl()['tt'][2:] #* ls * (ls+1)/2/np.pi
-Clte = CLASS_instance_current.raw_cl()['te'][2:] #* ls * (ls+1)/2/np.pi
-Clee = CLASS_instance_current.raw_cl()['ee'][2:] #* ls * (ls+1)/2/np.pi
+# Clte = CLASS_instance_current.raw_cl()['te'][2:] #* ls * (ls+1)/2/np.pi
+# Clee = CLASS_instance_current.raw_cl()['ee'][2:] #* ls * (ls+1)/2/np.pi
 ellmin=int(ls[0])
 
 TT2018 = PlanckLitePy(data_directory=os.path.join(MCMCmodulespath,'util','data'), year=2018, spectra='TT', use_low_ell_bins=False)
 
-chi2_planck_old = -2 * TT2018.loglike(Cltt, Clte, Clee, ellmin)
+chi2_planck_old = -2 * TT2018.loglike(Cltt, Clte=None, Clee=None, ellmin)
 
 
 chi2old = np.sum(list(chi2old1.values())) + chi2_planck_old
@@ -1079,10 +1079,10 @@ for ii in np.arange(1,int(niterations+1)):
             
             # ls = CLASS_instance_current.raw_cl()['ell'][2:]
             Cltt = CLASS_instance_current.raw_cl()['tt'][2:] #* ls * (ls+1)/2/np.pi
-            Clte = CLASS_instance_current.raw_cl()['te'][2:] #* ls * (ls+1)/2/np.pi
-            Clee = CLASS_instance_current.raw_cl()['ee'][2:] #* ls * (ls+1)/2/np.pi
+            # Clte = CLASS_instance_current.raw_cl()['te'][2:] #* ls * (ls+1)/2/np.pi
+            # Clee = CLASS_instance_current.raw_cl()['ee'][2:] #* ls * (ls+1)/2/np.pi
             # ellmin=int(ls[0])
-            chi2_planck_new = -2 * TT2018.loglike(Cltt, Clte, Clee, ellmin)
+            chi2_planck_new = -2 * TT2018.loglike(Cltt, Clte=None, Clee=None, ellmin)
             
             
             if np.isinf(chi2_planck_new) or np.isnan(chi2_planck_new):
